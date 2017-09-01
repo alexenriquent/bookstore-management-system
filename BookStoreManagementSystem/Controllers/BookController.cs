498448d9c19ccbab8d818eca5c35c63174361bd7
Thanat Chokwijitkul <alexenriquent@gmail.com>
2017-09-01 14:33:40 +1000

    Implemented a new project

--- BookStoreManagementSystem/Controllers/BookController.cs
+++ BookStoreManagementSystem/Controllers/BookController.cs
@@ -8,97 +8,80 @@ using System.Web;
 using System.Web.Mvc;
 using BookStoreManagementSystem.Models;
 
-namespace BookStoreManagementSystem.Controllers
-{
-    public class BookController : Controller
-    {
+namespace BookStoreManagementSystem.Controllers {
+
+    public class BookController : BaseController {
+
         private BookDBContext db = new BookDBContext();
 
         // GET: Book
-        public ActionResult Index()
-        {
+        public ActionResult Index() {
             return View(db.S4452232.ToList());
         }
 
         // GET: Book/Details/5
-        public ActionResult Details(string id)
-        {
-            if (id == null)
-            {
+        public ActionResult Details(string id) {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             S4452232 s4452232 = db.S4452232.Find(id);
-            if (s4452232 == null)
-            {
+            if (s4452232 == null) {
                 return HttpNotFound();
             }
             return View(s4452232);
         }
 
         // GET: Book/Create
-        public ActionResult Create()
-        {
+        public ActionResult Create() {
             return View();
         }
 
         // POST: Book/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Index,Name,Author,Year,Price,Stock")] S4452232 s4452232)
-        {
-            if (ModelState.IsValid)
-            {
+        public ActionResult Create([Bind(Include = "ID,Index,Name,Author,Year,Price,Stock")] S4452232 s4452232) {
+            if (ModelState.IsValid) {
                 db.S4452232.Add(s4452232);
                 db.SaveChanges();
+                UpdateDatabase();
+                SaveBooks(db.S4452232.ToList());
                 return RedirectToAction("Index");
             }
-
             return View(s4452232);
         }
 
         // GET: Book/Edit/5
-        public ActionResult Edit(string id)
-        {
-            if (id == null)
-            {
+        public ActionResult Edit(string id) {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             S4452232 s4452232 = db.S4452232.Find(id);
-            if (s4452232 == null)
-            {
+            if (s4452232 == null) {
                 return HttpNotFound();
             }
             return View(s4452232);
         }
 
         // POST: Book/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Index,Name,Author,Year,Price,Stock")] S4452232 s4452232)
-        {
-            if (ModelState.IsValid)
-            {
+        public ActionResult Edit([Bind(Include = "ID,Index,Name,Author,Year,Price,Stock")] S4452232 s4452232) {
+            if (ModelState.IsValid) {
                 db.Entry(s4452232).State = EntityState.Modified;
                 db.SaveChanges();
+                SaveBooks(db.S4452232.ToList());
                 return RedirectToAction("Index");
             }
             return View(s4452232);
         }
 
         // GET: Book/Delete/5
-        public ActionResult Delete(string id)
-        {
-            if (id == null)
-            {
+        public ActionResult Delete(string id) {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             S4452232 s4452232 = db.S4452232.Find(id);
-            if (s4452232 == null)
-            {
+            if (s4452232 == null) {
                 return HttpNotFound();
             }
             return View(s4452232);
@@ -107,21 +90,40 @@ namespace BookStoreManagementSystem.Controllers
         // POST: Book/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
-        {
+        public ActionResult DeleteConfirmed(string id) {
             S4452232 s4452232 = db.S4452232.Find(id);
             db.S4452232.Remove(s4452232);
             db.SaveChanges();
+            UpdateDatabase();
+            SaveBooks(db.S4452232.ToList());
             return RedirectToAction("Index");
         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        // GET: Book/DeleteBooks
+        public ActionResult DeleteBooks() {
+            ViewBag.books = db.S4452232.ToList();
+            return View(new S4452232());
+        }
+
+        // POST: Book/DeleteBooks/
+        [HttpPost]
+        public ActionResult DeleteBooks(string attribute, S4452232 book) {
+            switch (attribute) {
+                case "index": db.S4452232.RemoveRange(db.S4452232.Where(c => c.Index == book.Index)); break;
+                case "id": db.S4452232.RemoveRange(db.S4452232.Where(c => c.ID == book.ID)); break;
+                case "year": db.S4452232.RemoveRange(db.S4452232.Where(c => c.Year == book.Year)); break;
+            }
+            db.SaveChanges();
+            UpdateDatabase();
+            SaveBooks(db.S4452232.ToList());
+            return Redirect("/Book/DeleteBooks");
         }
     }
 }
--- BookStoreManagementSystem/Models/BookDBContext.cs
+++ BookStoreManagementSystem/Models/BookDBContext.cs
@@ -1,5 +1,6 @@
 namespace BookStoreManagementSystem.Models {
     using System;
+    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.Data.Entity;
     using System.Linq;
@@ -34,27 +35,40 @@ namespace BookStoreManagementSystem.Models {
     }
 
     public class BookDBContext : DbContext {
-        // Your context has been configured to use a 'BookDBContext' connection string from your application's 
-        // configuration file (App.config or Web.config). By default, this connection string targets the 
-        // 'BookStoreManagementSystem.Models.BookDBContext' database on your LocalDb instance. 
-        // 
-        // If you wish to target a different database and/or database provider, modify the 'BookDBContext' 
-        // connection string in the application configuration file.
-        public BookDBContext()
+
+        public BookDBContext() 
             : base("name=BookDBContext") {
+            Database.SetInitializer(new BookDBInitialiser());
         }
 
         public System.Data.Entity.DbSet<BookStoreManagementSystem.Models.S4452232> S4452232 { get; set; }
-
-        // Add a DbSet for each entity type that you want to include in your model. For more information 
-        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
-
-        // public virtual DbSet<MyEntity> MyEntities { get; set; }
     }
 
-    //public class MyEntity
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //}
+    public class BookDBInitialiser : DropCreateDatabaseAlways<BookDBContext> {
+
+        protected override void Seed(BookDBContext context) {
+            localhost.DataStorage storage = new localhost.DataStorage();
+            IList<S4452232> books = new List<S4452232>();
+            string[] bookList = storage.Read();
+
+            for (int i = 0; i < bookList.Length; i++) {
+                string[] elements = bookList[i].Split(',');
+                S4452232 book = new S4452232 {
+                    Index = i + 1,
+                    ID = elements[0],
+                    Name = elements[1],
+                    Author = elements[2],
+                    Year = int.Parse(elements[3]),
+                    Price = decimal.Parse(elements[4].Substring(1)),
+                    Stock = int.Parse(elements[5])
+                };
+                books.Add(book);
+            }
+
+            foreach (S4452232 book in books)
+                context.S4452232.Add(book);
+
+            base.Seed(context);
+        }
+    }
 }
